5d90683d82f7a57800b65c3831ccbc755ad89307
Anthony Garand <garand@garand.me>
2015-07-09 13:51:27 -0400

    Initial commit

---
+++ wp-config.php
@@ -0,0 +1,122 @@
+<?php
+
+require 'vendor/vlucas/phpdotenv/src/Dotenv.php';
+require 'vendor/vlucas/phpdotenv/src/Loader.php';
+require 'vendor/vlucas/phpdotenv/src/Validator.php';
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', getenv('DB_NAME'));
+
+/** MySQL database username */
+define('DB_USER', getenv('DB_USER'));
+
+/** MySQL database password */
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+
+/** MySQL hostname */
+define('DB_HOST', getenv('DB_HOST'));
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+// ** Configuration Settings ** //
+/** Make WordPress URL's dynamic */
+$base_path = substr(ABSPATH, strlen($_SERVER['DOCUMENT_ROOT']));
+define('WP_SITEURL', "http://${_SERVER['HTTP_HOST']}${base_path}");
+define('WP_HOME',    "http://${_SERVER['HTTP_HOST']}");
+
+/** The location of the wp-content folder */
+define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
+define( 'WP_CONTENT_URL', "http://${_SERVER['HTTP_HOST']}/wp-content" );
+
+/** Set Jetpack to debug mode */
+define ('JETPACK_DEV_DEBUG', true);
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+  define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+// ** Disable WordPress Update Notifications ** //
+function cvf_remove_wp_core_updates(){
+  global $wp_version;
+  return(object) array('last_checked' => time(),'version_checked' => $wp_version);
+}
+
+/** Core Notifications */
+add_filter('pre_site_transient_update_core','cvf_remove_wp_core_updates');
+/** Plugin Notifications */
+add_filter('pre_site_transient_update_plugins','cvf_remove_wp_core_updates');
+/** Theme Notifications */
+add_filter('pre_site_transient_update_themes','cvf_remove_wp_core_updates');
